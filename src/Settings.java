b3d59566b5c86f0ef6bf7fc8a15cc96c15cbbf24
Vision666 <bvision666@gmail.com>
2019-02-12 18:17:16 +0300

    Initial commit

---
+++ src/Settings.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class Settings {
+
+    // Токен Telegram бота
+    public String TgToken = "";
+
+    // ID чата в ТГ
+    public String TgChatId = "";
+
+    // Прокси для отправки сообщения в Telegram
+    public String ProxyTelegram = "";
+
+    // Минута матча, для отправки сообщения
+    //public int TimeSelect = 10;
+
+    // Владение мячом, минимум
+    public int PossessionMin = 65;
+
+    // Удар по воротам, минимум
+    public int TargetOnMin = 2;
+
+    // Удар мимо ворот, минимум
+    public int TargetOffMin = 5;
+
+    // И/ИЛИ
+    public logic Logic = logic.OR;
+
+    // Список прокси для сайта
+    private List<String> lstProxy =new List<String>();
+
+    // Логика условий
+    public enum logic {
+        AND,
+        OR
+    }
+}
