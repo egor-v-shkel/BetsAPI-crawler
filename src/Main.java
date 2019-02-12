66f8dedd5adf610871bb31a241fbf0f655120a23
Vision666 <bvision666@gmail.com>
2019-02-12 23:34:29 +0300

    Start realisation of method parseMatchSite

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Parser.parseMainPage();
+        //Parser.parseMainPage();
+        Parser.parseMatchSite();
 
     }
 
--- src/Parser.java
+++ src/Parser.java
@@ -10,9 +10,12 @@ import java.util.List;
 
 public class Parser {
 
-    public static void parseMainPage(){
+    /*public static void parseMainPage() {
 
-        //final String betsapiMainURL = "https://ru.betsapi.com/ci/soccer";
+        //read from url
+        //final String MAIN_URL = "https://ru.betsapi.com/ci/soccer";
+
+        //read from local file
         File input = new File("D:\\BetsAPI.html");
         Document doc = null;
         try {
@@ -20,25 +23,45 @@ public class Parser {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //List<ParseInfo> parsInfList = new List<ParseInfo>();
-        //Document doc  = null;
         Element tblInplayElement;
         String pageTitle = "";
         String matchURL = "";
         ArrayList<Element> elementList = new ArrayList<>(); //list of parsed elements
 
-        /*try {
+        *//*try {
             doc = Jsoup.connect(betsapiMainURL).get();
             pageTitle = doc.title();
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }*//*
 
         assert doc != null;
         tblInplayElement = doc.getElementsByAttributeValue("id", "tbl_inplay").first();
-        System.out.println(tblInplayElement);
-        System.out.println();
-        System.out.println("!!!"+tblInplayElement.select("a[id]").eachAttr("href"));
+        List<String> matchURLList = tblInplayElement.select("a[id]").eachAttr("href");
+        final String HOST_SITE = "https://ru.betsapi.com";
+
+        for (String urlAttributes : matchURLList
+        ) {
+            matchURL = HOST_SITE + urlAttributes;
+            parseMatchSite(matchURL);
+        }
+
+
+    }*/
+
+    public static void parseMatchSite() {
+
+        //String matchSite = "";
+
+        File input = new File("D:\\matchSite.html");
+        Document doc = null;
+        try {
+            doc = Jsoup.parse(input, "UTF-8", "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 
