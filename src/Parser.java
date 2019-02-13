f3684b7b004e58a59b204d38448f9770a50ab2f2
Vision666 <bvision666@gmail.com>
2019-02-13 12:36:53 +0300

    Add selection of main information in parseMatchSite method

--- src/Parser.java
+++ src/Parser.java
@@ -10,13 +10,14 @@ import java.util.List;
 
 public class Parser {
 
-    /*public static void parseMainPage() {
+    public static void parseMainPage() {
 
         //read from url
-        //final String MAIN_URL = "https://ru.betsapi.com/ci/soccer";
+        final String MAIN_URL = "https://ru.betsapi.com/ci/soccer";
 
         //read from local file
         File input = new File("D:\\BetsAPI.html");
+
         Document doc = null;
         try {
             doc = Jsoup.parse(input, "UTF-8", "");
@@ -28,12 +29,12 @@ public class Parser {
         String matchURL = "";
         ArrayList<Element> elementList = new ArrayList<>(); //list of parsed elements
 
-        *//*try {
-            doc = Jsoup.connect(betsapiMainURL).get();
+        try {
+            doc = Jsoup.connect(MAIN_URL).get();
             pageTitle = doc.title();
         } catch (IOException e) {
             e.printStackTrace();
-        }*//*
+        }
 
         assert doc != null;
         tblInplayElement = doc.getElementsByAttributeValue("id", "tbl_inplay").first();
@@ -43,17 +44,17 @@ public class Parser {
         for (String urlAttributes : matchURLList
         ) {
             matchURL = HOST_SITE + urlAttributes;
-            parseMatchSite(matchURL);
+            //parseMatchSite(matchURL);
         }
 
 
-    }*/
+    }
 
     public static void parseMatchSite() {
 
         //String matchSite = "";
 
-        File input = new File("D:\\matchSite.html");
+        File input = new File("e:\\zeno\\matchSite.html");
         Document doc = null;
         try {
             doc = Jsoup.parse(input, "UTF-8", "");
@@ -61,7 +62,12 @@ public class Parser {
             e.printStackTrace();
         }
 
-
+        //creating elements with main information
+        Elements infoInTr = doc.select("table.table-sm tr");
+        for (Element e:infoInTr
+             ) {
+            System.out.println("!!!Element!!!"+e);
+        }
 
     }
 
