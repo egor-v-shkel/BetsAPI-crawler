983961b71ad700cf0828114b0cd3e3c5c22f1588
Vision666 <bvision666@gmail.com>
2019-02-15 14:00:36 +0300

    implemented functionality of parser class

--- src/MatchInfo.java
+++ src/MatchInfo.java
@@ -5,31 +5,31 @@ public class MatchInfo {
     private String ClubName;
 
     // Голы
-    private String Goals;
+    private int Goals;
 
     // Угловые
-    private String Corners;
+    private int Corners;
 
     // Угловые в первом тайме
-    private String CornersHalf;
+    private int CornersHalf;
 
     // Желтые карточки
-    private String CardYellow;
+    private int CardYellow;
 
     // Красные карточки
-    private String CardRed;
+    private int CardRed;
 
     // Пенальти
-    private String Penalties;
+    private int Penalties;
 
     // Замены
-    private String Substitutions;
+    private int Substitutions;
 
     // Атака
-    private String Attacks;
+    private int Attacks;
 
     // Опасная Атака
-    private String AttacksDangerous;
+    private int AttacksDangerous;
 
     // Удары в створ ворот
     private int TargetOn;
@@ -40,4 +40,107 @@ public class MatchInfo {
     // Владение мячом, в %
     private int Possession;
 
+    public String getClubName() {
+        return ClubName;
+    }
+
+    public void setClubName(String clubName) {
+        ClubName = clubName;
+    }
+
+    public int getGoals() {
+        return Goals;
+    }
+
+    public void setGoals(int goals) {
+        Goals = goals;
+    }
+
+    public int getCorners() {
+        return Corners;
+    }
+
+    public void setCorners(int corners) {
+        Corners = corners;
+    }
+
+    public int getCornersHalf() {
+        return CornersHalf;
+    }
+
+    public void setCornersHalf(int cornersHalf) {
+        CornersHalf = cornersHalf;
+    }
+
+    public int getCardYellow() {
+        return CardYellow;
+    }
+
+    public void setCardYellow(int cardYellow) {
+        CardYellow = cardYellow;
+    }
+
+    public int getCardRed() {
+        return CardRed;
+    }
+
+    public void setCardRed(int cardRed) {
+        CardRed = cardRed;
+    }
+
+    public int getPenalties() {
+        return Penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        Penalties = penalties;
+    }
+
+    public int getSubstitutions() {
+        return Substitutions;
+    }
+
+    public void setSubstitutions(int substitutions) {
+        Substitutions = substitutions;
+    }
+
+    public int getAttacks() {
+        return Attacks;
+    }
+
+    public void setAttacks(int attacks) {
+        Attacks = attacks;
+    }
+
+    public int getAttacksDangerous() {
+        return AttacksDangerous;
+    }
+
+    public void setAttacksDangerous(int attacksDangerous) {
+        this.AttacksDangerous = attacksDangerous;
+    }
+
+    public int getTargetOn() {
+        return TargetOn;
+    }
+
+    public void setTargetOn(int targetOn) {
+        TargetOn = targetOn;
+    }
+
+    public int getTargetOff() {
+        return TargetOff;
+    }
+
+    public void setTargetOff(int targetOff) {
+        TargetOff = targetOff;
+    }
+
+    public int getPossession() {
+        return Possession;
+    }
+
+    public void setPossession(int possession) {
+        Possession = possession;
+    }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -70,7 +70,7 @@ public class Parser {
     public static void parseMatchSite() {
 
         //String matchSite = "";
-
+        MatchInfo info = new MatchInfo();
         File input = new File("d:\\matchSite.html");
         Document doc = null;
         try {
@@ -79,21 +79,103 @@ public class Parser {
             e.printStackTrace();
         }
 
-        //creating elements that contain target information
+        //selecting elements that contain target information
         Elements infoInTr = doc.select("table.table-sm tr");
+        //remove <span class="sr-only"> cause of repeating parameters with <div ... role="progressbar"...>
+        infoInTr.select("span.sr-only").remove();
+        ArrayList<ArrayList> paramArrList = new ArrayList<>();
+
         for (Element e : infoInTr
         ) {
-            String elementSpanL = "", elementSpanR = "";
-            Elements listTdElements = e.getElementsByTag("td");
-            String leftTeamVal = listTdElements.first().ownText();
-            String rightTeamVal = listTdElements.last().ownText();
-            Element leftTeamSpanElem = listTdElements.first();
-            Element rightTeamSpanVal = listTdElements.tagName("span").attr("class", "sr-only").first();
-            //String rightTeamSpanVal = leftTeamSpanElem.selectFirst("span.sr-only").ownText();
-            //System.out.println("Left team span val " + leftTeamSpanVal);
-            //System.out.println("Right team span val " + rightTeamSpanVal);
-            System.out.println(rightTeamSpanVal);
+            Elements tdTagElements = e.select("td");
+            ArrayList<String> paramInfoBlock = new ArrayList<>();
+            if (tdTagElements.hasText()) {
+                for (Element tdElement : tdTagElements
+                ) {
+                    paramInfoBlock.add(tdElement.text());
+                }
+                paramArrList.add(paramInfoBlock);
+            }
 
+        }
+
+        for (ArrayList param : paramArrList
+        ) {
+            String header =(String) param.get(1);
+            Object leftTeamParam = param.get(0);
+            Object rightTeamParam = param.get(2);
+
+            switch (header)
+            {
+                case "":
+                    info.setClubName((String) leftTeamParam);
+                    info.setClubName((String) rightTeamParam);
+                    break;
+
+                case "Goals":
+                    info.setGoals((int) leftTeamParam);
+                    info.setGoals((int) rightTeamParam);
+                    break;
+
+                case "Corners":
+                    info.setCorners((int) leftTeamParam);
+                    info.setCorners((int) rightTeamParam);
+                    break;
+
+                case "Corners (Half)":
+                    info.setCornersHalf((int) leftTeamParam);
+                    info.setCornersHalf((int) rightTeamParam);
+                    break;
+
+                case "Желтые карточки":
+                    info.setCardYellow((int) leftTeamParam);
+                    info.setCardYellow((int) rightTeamParam);
+                    break;
+
+                case "Красные карточки":
+                    info.setCardRed((int) leftTeamParam);
+                    info.setCardRed((int) rightTeamParam);
+                    break;
+
+                case "Пенальти":
+                    info.setPenalties((int) leftTeamParam);
+                    info.setPenalties((int) rightTeamParam);
+                    break;
+
+                case "Замены":
+                    info.setSubstitutions((int) leftTeamParam);
+                    info.setSubstitutions((int) rightTeamParam);
+                    break;
+
+                case "Атака":
+                    info.setAttacks((int) leftTeamParam);
+                    info.setAttacks((int) rightTeamParam);
+                    break;
+
+                case "Опасная Атака":
+                    info.setAttacksDangerous((int) leftTeamParam);
+                    info.setAttacksDangerous((int) rightTeamParam);
+                    break;
+
+
+                case "Удары в створ ворот":
+                    info.setTargetOn((int) leftTeamParam);
+                    info.setTargetOn((int) rightTeamParam);
+                    break;
+
+                case "Удары мимо ворот":
+                    info.setTargetOff((int) leftTeamParam);
+                    info.setTargetOff((int) rightTeamParam);
+                    break;
+
+                case "Владение мячом":
+                    info.setPossession((int) leftTeamParam);
+                    info.setPossession((int) rightTeamParam);
+                    break;
+
+            }
+            System.out.println("header"+header);
+            System.out.println("List ==>" + param);
         }
 
     }
