9f4cf958de4d8af9b1fcd823dd87fa6880126bd0
VIsion666 <bvision666@gmail.com>
2019-02-23 00:47:36 +0300

    Revert "Converted to a Maven project"
    This reverts commit c5368bfe

--- src/main/java/Proxy.java
+++ src/main/java/Proxy.java
@@ -1,35 +1,8 @@
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.glassfish.jersey.server.Uri;
 
 public class Proxy {
 
-    String ip;
-    int port;
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
     static void getProxyList(){
-
-        try {
-            URL url = new URL("http://pubproxy.com/api/proxy?format=json&type=http&https=true&last_check=60&speed=25&limit=20&user_agent=true&referer=true&country=US,RU");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
 
     }
 
