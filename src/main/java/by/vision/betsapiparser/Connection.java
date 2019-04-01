7e07add2dc3b9234a40417acf5758e21ef3c5029
Vision666 <bvision666@gmail.com>
2019-04-01 14:13:05 +0300

    commit

--- src/main/java/by/vision/betsapiparser/Connection.java
+++ src/main/java/by/vision/betsapiparser/Connection.java
@@ -4,52 +4,64 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.net.Proxy;
 
 public class Connection {
 
     String request;
     org.jsoup.Connection.Response response;
-    String proxy;
+    Proxy proxy;
     boolean useProxy;
-    int numTries = 5;
+    private int NUM_TRIES = 5;
+    Document doc;
+    org.jsoup.Connection preset = Jsoup.connect(request)
+            .timeout(Settings.proxyTimeout)
+            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36")
+            .referrer("http://www.google.com");
+
 
     Connection(String request){
         this.request = request;
     }
-    Connection(String request, boolean useProxy){
-        this.request = request;
-        getProxy();
 
-    }
 
-    Connection(String request, String proxy){
-        this.request = request;
-        this.proxy = proxy;
-
-    }
-
-    private void getProxy() {
-
-    }
-
-    private void connect(String request){
+    private org.jsoup.Connection.Response connect(){
         while (!FXMLController.bStop) {
             try {
-                response = Jsoup.connect(request)
-                        .timeout(Settings.proxyTimeout)
-                        .proxy(ip, port)
-                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36")
-                        .referrer("http://www.google.com")
+                response = preset
                         //.ignoreHttpErrors(true)
+                        .execute();
+                break;
+            } catch (IOException e) {
+                MyLogger.ROOT_LOGGER.info("Unsuccessful connection for proxy "+ip+":"+port+"\n", e);
+                if (--NUM_TRIES <= 0) try {
+                    throw e;
+                } catch (IOException e1) {
+                    MyLogger.ROOT_LOGGER.info("Reconnection attempt №"+ NUM_TRIES);
+                }
+            }
+        }
+
+
+
+
+    }
+
+    private org.jsoup.Connection.Response connect(Proxy proxy){
+        while (!FXMLController.bStop) {
+            try {
+                response = preset
+                        //.ignoreHttpErrors(true)
+                        .proxy(ip, port)
                         .execute();
 
                 break;
             } catch (IOException e) {
                 MyLogger.ROOT_LOGGER.info("Unsuccessful connection for proxy "+ip+":"+port+"\n", e);
-                if (--numTries <= 0) try {
+                if (--NUM_TRIES <= 0) try {
                     throw e;
                 } catch (IOException e1) {
-                    MyLogger.ROOT_LOGGER.info("Reconnection attempt №"+numTries);
+                    MyLogger.ROOT_LOGGER.info("Reconnection attempt №"+ NUM_TRIES);
                     proxy.refresh();
                     ip = proxy.getIp();
                     port = proxy.getPort();
@@ -57,7 +69,24 @@ public class Connection {
             }
         }
 
-        Document doc = null;
+
+    }
+
+    private void connect(boolean autoProxy){
+        while (!FXMLController.bStop) {
+            response = autoProxy ? connect(getProxy()) : connect();
+        }
+    }
+
+    private Proxy getProxy() {
+
+    }
+
+    private String getResponse(){
+
+    }
+
+    private void respToDoc{
         try {
             doc = response.parse();
         } catch (IOException e) {
@@ -66,13 +95,6 @@ public class Connection {
         } finally {
             return doc;
         }
-
-
     }
-
-    private String getResponse(){
-
-    }
-
 
 }
---
+++ src/main/java/by/vision/betsapiparser/ProxyAdress.java
@@ -0,0 +1,4 @@
+package by.vision.betsapiparser;
+
+public class ProxyAdress {
+}
