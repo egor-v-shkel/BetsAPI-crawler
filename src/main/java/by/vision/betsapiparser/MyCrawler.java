4e9006ad11faf4c3a47c4e1be10575f7213653b1
VIsion666 <bvision666@gmail.com>
2019-04-06 16:46:45 +0300

    Add Crawl4J API

---
+++ src/main/java/by/vision/betsapiparser/MyCrawler.java
@@ -0,0 +1,91 @@
+package by.vision.betsapiparser;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+
+import org.apache.http.Header;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class MyCrawler extends WebCrawler {
+
+    private static final Pattern IMAGE_EXTENSIONS = Pattern.compile(".*\\.(bmp|gif|jpg|png)$");
+
+    private final AtomicInteger numSeenImages;
+
+    /**
+     * Creates a new crawler instance.
+     *
+     * @param numSeenImages This is just an example to demonstrate how you can pass objects to crawlers. In this
+     * example, we pass an AtomicInteger to all crawlers and they increment it whenever they see a url which points
+     * to an image.
+     */
+    public MyCrawler(AtomicInteger numSeenImages) {
+        this.numSeenImages = numSeenImages;
+    }
+
+    /**
+     * You should implement this function to specify whether the given url
+     * should be crawled or not (based on your crawling logic).
+     */
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+        String href = url.getURL().toLowerCase();
+        // Ignore the url if it has an extension that matches our defined set of image extensions.
+        if (IMAGE_EXTENSIONS.matcher(href).matches()) {
+            numSeenImages.incrementAndGet();
+            return false;
+        }
+
+        // Only accept the url if it is in the "www.ics.uci.edu" domain and protocol is "http".
+        return href.startsWith("https://www.ics.uci.edu/");
+    }
+
+    /**
+     * This function is called when a page is fetched and ready to be processed
+     * by your program.
+     */
+    @Override
+    public void visit(Page page) {
+        int docid = page.getWebURL().getDocid();
+        String url = page.getWebURL().getURL();
+        String domain = page.getWebURL().getDomain();
+        String path = page.getWebURL().getPath();
+        String subDomain = page.getWebURL().getSubDomain();
+        String parentUrl = page.getWebURL().getParentUrl();
+        String anchor = page.getWebURL().getAnchor();
+
+        logger.debug("Docid: {}", docid);
+        logger.info("URL: {}", url);
+        logger.debug("Domain: '{}'", domain);
+        logger.debug("Sub-domain: '{}'", subDomain);
+        logger.debug("Path: '{}'", path);
+        logger.debug("Parent page: {}", parentUrl);
+        logger.debug("Anchor text: {}", anchor);
+
+        if (page.getParseData() instanceof HtmlParseData) {
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+            String text = htmlParseData.getText();
+            String html = htmlParseData.getHtml();
+            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+            logger.debug("Text length: {}", text.length());
+            logger.debug("Html length: {}", html.length());
+            logger.debug("Number of outgoing links: {}", links.size());
+        }
+
+        Header[] responseHeaders = page.getFetchResponseHeaders();
+        if (responseHeaders != null) {
+            logger.debug("Response headers:");
+            for (Header header : responseHeaders) {
+                logger.debug("\t{}: {}", header.getName(), header.getValue());
+            }
+        }
+
+        logger.debug("=============");
+    }
+}
---
+++ src/main/java/by/vision/betsapiparser/MyCrawlerController.java
@@ -0,0 +1,77 @@
+package by.vision.betsapiparser;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class MyCrawlerController {
+
+    public static void main(String[] args) throws Exception {
+        CrawlConfig config = new CrawlConfig();
+
+        // Set the folder where intermediate crawl data is stored (e.g. list of urls that are extracted from previously
+        // fetched pages and need to be crawled later).
+        config.setCrawlStorageFolder("C:\\temp\\");
+
+        // Be polite: Make sure that we don't send more than 1 request per second (1000 milliseconds between requests).
+        // Otherwise it may overload the target servers.
+        config.setPolitenessDelay(1000);
+
+        // You can set the maximum crawl depth here. The default value is -1 for unlimited depth.
+        config.setMaxDepthOfCrawling(2);
+
+        // You can set the maximum number of pages to crawl. The default value is -1 for unlimited number of pages.
+        config.setMaxPagesToFetch(1000);
+
+        // Should binary data should also be crawled? example: the contents of pdf, or the metadata of images etc
+        config.setIncludeBinaryContentInCrawling(false);
+
+        // Do you need to set a proxy? If so, you can use:
+        // config.setProxyHost("proxyserver.example.com");
+        // config.setProxyPort(8080);
+
+        // If your proxy also needs authentication:
+        // config.setProxyUsername(username); config.getProxyPassword(password);
+
+        // This config parameter can be used to set your crawl to be resumable
+        // (meaning that you can resume the crawl from a previously
+        // interrupted/crashed crawl). Note: if you enable resuming feature and
+        // want to start a fresh crawl, you need to delete the contents of
+        // rootFolder manually.
+        config.setResumableCrawling(false);
+
+        // Instantiate the controller for this crawl.
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        // For each crawl, you need to add some seed urls. These are the first
+        // URLs that are fetched and then the crawler starts following links
+        // which are found in these pages
+        controller.addSeed("https://www.ics.uci.edu/");
+        controller.addSeed("https://www.ics.uci.edu/~lopes/");
+        controller.addSeed("https://www.ics.uci.edu/~welling/");
+
+        // Number of threads to use during crawling. Increasing this typically makes crawling faster. But crawling
+        // speed depends on many other factors as well. You can experiment with this to figure out what number of
+        // threads works best for you.
+        int numberOfCrawlers = 8;
+
+        // To demonstrate an example of how you can pass objects to crawlers, we use an AtomicInteger that crawlers
+        // increment whenever they see a url which points to an image.
+        AtomicInteger numSeenImages = new AtomicInteger();
+
+        // The factory which creates instances of crawlers.
+        CrawlController.WebCrawlerFactory<MyCrawler> factory = () -> new MyCrawler(numSeenImages);
+
+        // Start the crawl. This is a blocking operation, meaning that your code
+        // will reach the line after this only when crawling is finished.
+        controller.start(factory, numberOfCrawlers);
+    }
+
+}
